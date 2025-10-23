6ad2fe3bdd5b89138d9dbde59a120b8447794e65
Mrbysco <Mrbysco@users.noreply.github.com>
2025-10-23 18:57:43 +0200

    Sync the allyship effect to other clients

--- src/main/java/com/mrbysco/allyeffect/handler/EffectHandler.java
+++ src/main/java/com/mrbysco/allyeffect/handler/EffectHandler.java
@@ -4,12 +4,16 @@ import com.mrbysco.allyeffect.config.AllyConfig;
 import com.mrbysco.allyeffect.effect.AllyEffect;
 import com.mrbysco.allyeffect.registry.AllyRegistry;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.game.ClientboundRemoveMobEffectPacket;
+import net.minecraft.network.protocol.game.ClientboundUpdateMobEffectPacket;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.MobEffectEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -66,6 +70,7 @@ public class EffectHandler {
 	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
 		if (event.phase == TickEvent.Phase.START && event.side.isServer()) {
 			Player player = event.player;
+			if (!(player instanceof ServerPlayer serverPlayer)) return;
 			CompoundTag persistentData = player.getPersistentData();
 			if (persistentData.getBoolean("allyeffectDisabled")) {
 				setAllyActive(player.getUUID(), false);
@@ -77,7 +82,53 @@ public class EffectHandler {
 				if (nearbyEntities.isEmpty() && AllyConfig.COMMON.activateOnlyInRange.get()) // Check for nearby entities
 					return;
 
-				player.forceAddEffect(new MobEffectInstance(AllyRegistry.ALLY.get(), 20), null);
+				MobEffectInstance instance = new MobEffectInstance(AllyRegistry.ALLY.get(), 20, 0, true, false);
+				player.addEffect(instance, player);
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public static void onEffectAdded(MobEffectEvent.Added event) {
+		MobEffectInstance instance = event.getEffectInstance();
+		LivingEntity effectEntity = event.getEntity();
+		if (effectEntity instanceof ServerPlayer serverPlayer && instance.getEffect() == AllyRegistry.ALLY.get()) {
+			ServerLevel serverLevel = serverPlayer.serverLevel();
+			for (Entity entity : serverLevel.getEntities().getAll()) {
+				if (entity instanceof ServerPlayer otherPlayer) {
+					// Sync the effect to other players
+					otherPlayer.connection.send(new ClientboundUpdateMobEffectPacket(serverPlayer.getId(), instance));
+				}
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public static void onEffectRemove(MobEffectEvent.Remove event) {
+		MobEffectInstance instance = event.getEffectInstance();
+		LivingEntity effectEntity = event.getEntity();
+		if (effectEntity instanceof ServerPlayer serverPlayer && instance.getEffect() == AllyRegistry.ALLY.get()) {
+			ServerLevel serverLevel = serverPlayer.serverLevel();
+			for (Entity entity : serverLevel.getEntities().getAll()) {
+				if (entity instanceof ServerPlayer otherPlayer) {
+					// Sync the removed effect to other players
+					otherPlayer.connection.send(new ClientboundRemoveMobEffectPacket(serverPlayer.getId(), AllyRegistry.ALLY.get()));
+				}
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public static void onEffectExpired(MobEffectEvent.Expired event) {
+		MobEffectInstance instance = event.getEffectInstance();
+		LivingEntity effectEntity = event.getEntity();
+		if (effectEntity instanceof ServerPlayer serverPlayer && instance.getEffect() == AllyRegistry.ALLY.get()) {
+			ServerLevel serverLevel = serverPlayer.serverLevel();
+			for (Entity entity : serverLevel.getEntities().getAll()) {
+				if (entity instanceof ServerPlayer otherPlayer) {
+					// Sync the effect expiring to other players
+					otherPlayer.connection.send(new ClientboundRemoveMobEffectPacket(serverPlayer.getId(), AllyRegistry.ALLY.get()));
+				}
 			}
 		}
 	}
